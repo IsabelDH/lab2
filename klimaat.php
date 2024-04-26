06a7adc3206cf8d9274d90efdbfd2933f9039889
IsabelDH <isabel.de.haes@hotmail.com>
2024-04-26 17:17:12 +0200

    klimaat inorde

--- klimaat.php
+++ klimaat.php
@@ -7,12 +7,56 @@
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/general.css">
     <link rel="stylesheet" href="styles/header.css">
-    <link rel="stylesheet" href="styles/detailpagina.css">
+    <link rel="stylesheet" href="styles/detailpagina4.css">
     <title>Klimaat</title>
 </head>
 <body>
     <?php include_once("nav.inc.php") ?> 
     <?php include_once("header.inc.php") ?>
+    <div class="artikels">
+        <div class="artikel_div">
+            <div class="image">
+                <img src="images/imageklimaat1.png" alt="">
+                <div class="like-button" onclick="toggleLike()">
+                    <img id="heart-icon" src="images/unlikeheart.png" alt="unlike">
+                </div>
+            </div>
+            <div class="article-content">
+                <div class="title">
+                <h2>Duurzaamheid in jouw huis</h2>
+                    <p>22/04/2024</p> 
+                </div>
+            <div class="text">
+                <p>Maak van je (ver)bouwproject een klimaatvriendelijke missie met deze tips voor duurzame keuzes en praktische oplossingen.
+                </p>
+            </div>
+                <a href="duurzaamhuis.php">Bekijk</a>
+            </div>
+        </div>
+
+        <div class="artikel_div">
+            <div class="image">
+                <img src="images/imageklimaat2.png" alt="">
+                <div class="like-button2" onclick="toggleLike2()">
+                    <img id="heart-icon2" src="images/unlikeheart.png" alt="unlike">
+                </div>
+            </div>
+            <div class="article-content">
+                <div class="title">
+                <h2>Oprichting van de Wetenschappelijke Klimaatraad</h2>
+                    <p>05/04/2024</p> 
+                </div>
+            <div class="text">
+                <p>Het ontwerp van koninklijk besluit introduceert een onafhankelijke Wetenschappelijke Klimaatraad voor evaluatie en opvolging van het federale klimaatbeleid, met bevoegdheid tot deskundigenraadpleging en onderzoeksrichtlijnen.
+                </p>
+            </div>
+                <a href="#">Bekijk</a>
+            </div>
+        </div>
+
+    </div>
+
+
     <?php include_once("footer.inc.php") ?>
 
     <script src="js/chatbot.js"></script>
