47f924d8577d0cdd2f89942cdd99f86eebfb3f52
IsabelDH <isabel.de.haes@hotmail.com>
2024-04-26 13:54:03 +0200

    footer ok

--- footer.inc.php
+++ footer.inc.php
@@ -1,5 +1,8 @@
-<footer>
-    <img src="#" alt="">
-    <img src="#" alt="">
-    <p>&copy Gemaakt door Hannelore Van Buynderen & Isabel De Haes</p>
+<footer class="footer">
+    <div class="logos">
+        <a href="https://www.vlaanderen.be/"><img width="100px" src="images/vlaamseoverheid.png" alt="vlaamse overheid website"></a>
+        <a href="https://www.belgium.be/nl"><img width="60px" src="images/belgium.be.png" alt="belgium website"></a>
+    </div>
+    
+    <p class="end">&copyGemaakt door Hannelore Van Buynderen & Isabel De Haes</p>
 </footer>
