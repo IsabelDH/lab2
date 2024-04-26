7b0c65f8395931d5593a6a4eb89a8120ea40bb30
IsabelDH <isabel.de.haes@hotmail.com>
2024-04-26 18:29:49 +0200

    meldingen klaar

--- meldingen.php
+++ meldingen.php
@@ -4,13 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/normalize.css">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/meldingen.css">
     <link rel="stylesheet" href="styles/general.css">
   
     <title>Meldingen</title>
 </head>
 <body>
     <?php include_once("nav.inc.php") ?> 
+
+    <h1>Meldingen</h1>
+    <div class="text">
+      <p>Oh, sorry nog geen meldingen voor jou!</p>
+        <p>Wil je meldingen krijgen over bepaalde artikels? 
+        Meld je dan snel aan via login.</p>  
+    </div>
+    
+
+    <img class="image" src="images/melding kat.png" alt="">
 
     <?php include_once("footer.inc.php") ?>
 
