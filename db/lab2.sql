49005514e91f9889352ed6c51d94d61fdb3dc780
IsabelDH <isabel.de.haes@hotmail.com>
2024-04-30 17:49:07 +0200

    databank

--- db/lab2.sql
+++ db/lab2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 30, 2024 at 12:46 PM
+-- Generation Time: Apr 30, 2024 at 03:48 PM
 -- Server version: 5.7.24
--- PHP Version: 8.0.1
+-- PHP Version: 8.1.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,7 +45,6 @@ CREATE TABLE `artikel` (
 INSERT INTO `artikel` (`id`, `naam`, `inhoud`, `datum`, `foto`, `favoriet`, `thema`, `link`) VALUES
 (1, 'Invoering elektronische procesvoering voor het Grondwettelijk Hof', 'De ministerraad keurt een ontwerp van koninklijk besluit goed voor de eerste fase van elektronische procesvoering bij het Grondwettelijk Hof, waarbij verzoekschriften en stukken elektronisch kunnen worden ingediend via een platform op de website van het Hof.', '29-03-2024', 'image1Politiek.png', 0, 'politiek', ''),
 (2, 'Verkiezingen', 'Jouw stem telt. Met die drie woorden zullen heel wat politieke partijen jou proberen te overtuigen om op hen te stemmen bij de verkiezingen van 9 juni. Eens iedereen gestemd heeft, worden de stemmen omgezet in zetels. In het federaal parlement zijn er bijvoorbeeld 150 zetels, in het Vlaams 124, in het Brussels 89.', '06-04-2024', 'image2politiek.png', 1, 'politiek', 'Verkiezingen.php'),
-(3, 'Europese verkiezingen', 'Op 9 juni 2024 worden 150 leden van de Kamer van Volksvertegenwoordigers verkozen in elf kieskringen, één per provincie en de kieskring Brussel-Hoofdstad. De 60 leden van de Senaat worden niet rechtstreeks gekozen, maar via de deelstaatparlementen. Deze verkiezingen markeren het begin van de 56ste zittingsperiode van het Federaal Parlement van België.', '06-04-2024', 'image3Politiek.png', 0, 'politiek', ''),
 (4, 'Begroting ', 'België sluit 2023 af met een begrotingstekort dat 6,5 miljard euro kleiner is dan gevreesd, vooral dankzij een sterkere economische groei, maar het tekort blijft nog steeds oplopen in vergelijking met voorgaande jaren.', '04-02-2024', 'imageEconomie1.png', 1, 'economie', 'begroting.php'),
 (5, 'De pensioenhervorming is gestemd', 'RSVZ en de Pensioendienst lanceren proactieve campagne voor pensioenbonus met slogan \'En jij, hoeveel doe jij erbij?\'', '05-04-2024', 'imageEconomie2.png', 0, 'economie', ''),
 (6, 'Faillissement Van Hool', 'Faillissement van busbouwer Van Hool dreigt Vlaanderen 18 tot 20 miljoen euro te kosten.', '02-04-2024', 'imageEconomie3.png', 0, 'economie', ''),
