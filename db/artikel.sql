19d347f0be33ed3d3adcd5749d9990f027c46bdc
Hannelorevb04 <hannelorevb@hotmail.be>
2024-04-30 14:48:16 +0200

    db

--- db/artikel.sql
+++ db/artikel.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Gegenereerd op: 28 apr 2024 om 22:28
--- Serverversie: 5.7.24
--- PHP-versie: 8.0.1
+-- Generation Time: Apr 30, 2024 at 12:44 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,51 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `artikel`
+-- Table structure for table `artikel`
 --
 
 CREATE TABLE `artikel` (
   `id` int(11) NOT NULL,
   `naam` varchar(300) NOT NULL,
   `inhoud` varchar(600) NOT NULL,
-  `datum` date DEFAULT NULL,
+  `datum` varchar(10) DEFAULT NULL,
   `foto` varchar(300) DEFAULT NULL,
   `favoriet` tinyint(1) DEFAULT '0',
-  `thema` varchar(300) NOT NULL
+  `thema` varchar(300) NOT NULL,
+  `link` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Gegevens worden geëxporteerd voor tabel `artikel`
+-- Dumping data for table `artikel`
 --
 
-INSERT INTO `artikel` (`id`, `naam`, `inhoud`, `datum`, `foto`, `favoriet`, `thema`) VALUES
-(1, 'Invoering elektronische procesvoering voor het Grondwettelijk Hof', 'De ministerraad keurt een ontwerp van koninklijk besluit goed voor de eerste fase van elektronische procesvoering bij het Grondwettelijk Hof, waarbij verzoekschriften en stukken elektronisch kunnen worden ingediend via een platform op de website van het Hof.', '2024-03-29', NULL, 0, ''),
-(2, 'Federale verkiezingen', 'Jouw stem telt. Met die drie woorden zullen heel wat politieke partijen jou proberen te overtuigen om op hen te stemmen bij de verkiezingen van 9 juni. Eens iedereen gestemd heeft, worden de stemmen omgezet in zetels. In het federaal parlement zijn er bijvoorbeeld 150 zetels, in het Vlaams 124, in het Brussels 89.', '2024-04-06', NULL, 0, ''),
-(3, 'Europese verkiezingen', 'Op 9 juni 2024 worden 150 leden van de Kamer van Volksvertegenwoordigers verkozen in elf kieskringen, één per provincie en de kieskring Brussel-Hoofdstad. De 60 leden van de Senaat worden niet rechtstreeks gekozen, maar via de deelstaatparlementen. Deze verkiezingen markeren het begin van de 56ste zittingsperiode van het Federaal Parlement van België.', '2024-04-06', NULL, 0, ''),
-(4, 'Begroting ', '', NULL, NULL, 0, ''),
-(5, 'De pensioenhervorming is gestemd', '', NULL, NULL, 0, ''),
-(6, 'Faillissement Van Hool', '', NULL, NULL, 0, ''),
-(7, 'Betaalbare psychologische zorg', '', NULL, NULL, 0, ''),
-(8, 'De jungle van de medische platformen', '', NULL, NULL, 0, ''),
-(9, 'Duurzaamheid in jouw huis', '', NULL, NULL, 0, ''),
-(10, 'Oprichting van de Wetenschappelijke Klimaatraad', '', NULL, NULL, 0, '');
+INSERT INTO `artikel` (`id`, `naam`, `inhoud`, `datum`, `foto`, `favoriet`, `thema`, `link`) VALUES
+(1, 'Invoering elektronische procesvoering voor het Grondwettelijk Hof', 'De ministerraad keurt een ontwerp van koninklijk besluit goed voor de eerste fase van elektronische procesvoering bij het Grondwettelijk Hof, waarbij verzoekschriften en stukken elektronisch kunnen worden ingediend via een platform op de website van het Hof.', '29-03-2024', 'image1Politiek.png', 0, 'politiek', ''),
+(2, 'Verkiezingen', 'Jouw stem telt. Met die drie woorden zullen heel wat politieke partijen jou proberen te overtuigen om op hen te stemmen bij de verkiezingen van 9 juni. Eens iedereen gestemd heeft, worden de stemmen omgezet in zetels. In het federaal parlement zijn er bijvoorbeeld 150 zetels, in het Vlaams 124, in het Brussels 89.', '06-04-2024', 'image2politiek.png', 1, 'politiek', 'Verkiezingen.php'),
+(3, 'Europese verkiezingen', 'Op 9 juni 2024 worden 150 leden van de Kamer van Volksvertegenwoordigers verkozen in elf kieskringen, één per provincie en de kieskring Brussel-Hoofdstad. De 60 leden van de Senaat worden niet rechtstreeks gekozen, maar via de deelstaatparlementen. Deze verkiezingen markeren het begin van de 56ste zittingsperiode van het Federaal Parlement van België.', '06-04-2024', 'image3Politiek.png', 0, 'politiek', ''),
+(4, 'Begroting ', 'België sluit 2023 af met een begrotingstekort dat 6,5 miljard euro kleiner is dan gevreesd, vooral dankzij een sterkere economische groei, maar het tekort blijft nog steeds oplopen in vergelijking met voorgaande jaren.', '04-02-2024', 'imageEconomie1.png', 1, 'economie', 'begroting.php'),
+(5, 'De pensioenhervorming is gestemd', 'RSVZ en de Pensioendienst lanceren proactieve campagne voor pensioenbonus met slogan \'En jij, hoeveel doe jij erbij?\'', '05-04-2024', 'imageEconomie2.png', 0, 'economie', ''),
+(6, 'Faillissement Van Hool', 'Faillissement van busbouwer Van Hool dreigt Vlaanderen 18 tot 20 miljoen euro te kosten.', '02-04-2024', 'imageEconomie3.png', 0, 'economie', ''),
+(7, 'Betaalbare psychologische zorg', 'Toegankelijke psychologische zorg: een stap naar welzijn voor iedereen.', '09-03-2024', 'imageGzondheid1.png', 0, 'gezondheid', ''),
+(8, 'De jungle van de medische platformen', 'Ontdek de weg naar jouw medische gegevens - helder, veilig en eenvoudig.', '11-04-2024', 'imageGezondheid2.png', 0, 'gezondheid', ''),
+(9, 'Duurzaamheid in jouw huis', 'Maak van je (ver)bouwproject een klimaatvriendelijke missie met deze tips voor duurzame keuzes en praktische oplossingen.', '22-04-2024', 'imageklimaat1.png', 1, 'klimaat', ''),
+(10, 'Oprichting van de Wetenschappelijke Klimaatraad', 'Het ontwerp van koninklijk besluit introduceert een onafhankelijke Wetenschappelijke Klimaatraad voor evaluatie en opvolging van het federale klimaatbeleid, met bevoegdheid tot deskundigenraadpleging en onderzoeksrichtlijnen.', '05-04-2024', 'imageKlimaat2.png', 0, 'klimaat', '');
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `artikel`
+-- Indexes for table `artikel`
 --
 ALTER TABLE `artikel`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `artikel`
+-- AUTO_INCREMENT for table `artikel`
 --
 ALTER TABLE `artikel`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
--- db/lab2.sql
+++ db/lab2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Gegenereerd op: 30 apr 2024 om 07:37
--- Serverversie: 5.7.24
--- PHP-versie: 8.0.1
+-- Generation Time: Apr 30, 2024 at 12:46 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,14 +24,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `artikel`
+-- Table structure for table `artikel`
 --
 
 CREATE TABLE `artikel` (
   `id` int(11) NOT NULL,
   `naam` varchar(300) NOT NULL,
   `inhoud` varchar(600) NOT NULL,
-  `datum` date DEFAULT NULL,
+  `datum` varchar(10) DEFAULT NULL,
   `foto` varchar(300) DEFAULT NULL,
   `favoriet` tinyint(1) DEFAULT '0',
   `thema` varchar(300) NOT NULL,
@@ -39,25 +39,25 @@ CREATE TABLE `artikel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Gegevens worden geëxporteerd voor tabel `artikel`
+-- Dumping data for table `artikel`
 --
 
 INSERT INTO `artikel` (`id`, `naam`, `inhoud`, `datum`, `foto`, `favoriet`, `thema`, `link`) VALUES
-(1, 'Invoering elektronische procesvoering voor het Grondwettelijk Hof', 'De ministerraad keurt een ontwerp van koninklijk besluit goed voor de eerste fase van elektronische procesvoering bij het Grondwettelijk Hof, waarbij verzoekschriften en stukken elektronisch kunnen worden ingediend via een platform op de website van het Hof.', '2024-03-29', 'image1Politiek.png', 0, 'politiek', ''),
-(2, 'Federale verkiezingen', 'Jouw stem telt. Met die drie woorden zullen heel wat politieke partijen jou proberen te overtuigen om op hen te stemmen bij de verkiezingen van 9 juni. Eens iedereen gestemd heeft, worden de stemmen omgezet in zetels. In het federaal parlement zijn er bijvoorbeeld 150 zetels, in het Vlaams 124, in het Brussels 89.', '2024-04-06', 'image2politiek.png', 1, 'politiek', 'federaleverkiezingen.php'),
-(3, 'Europese verkiezingen', 'Op 9 juni 2024 worden 150 leden van de Kamer van Volksvertegenwoordigers verkozen in elf kieskringen, één per provincie en de kieskring Brussel-Hoofdstad. De 60 leden van de Senaat worden niet rechtstreeks gekozen, maar via de deelstaatparlementen. Deze verkiezingen markeren het begin van de 56ste zittingsperiode van het Federaal Parlement van België.', '2024-04-06', 'image3Politiek.png', 0, 'politiek', ''),
-(4, 'Begroting ', 'België sluit 2023 af met een begrotingstekort dat 6,5 miljard euro kleiner is dan gevreesd, vooral dankzij een sterkere economische groei, maar het tekort blijft nog steeds oplopen in vergelijking met voorgaande jaren.', '2024-02-04', 'imageEconomie1.png', 1, 'economie', ''),
-(5, 'De pensioenhervorming is gestemd', 'RSVZ en de Pensioendienst lanceren proactieve campagne voor pensioenbonus met slogan \'En jij, hoeveel doe jij erbij?\'', '2024-04-05', 'imageEconomie2.png', 0, 'economie', ''),
-(6, 'Faillissement Van Hool', 'Faillissement van busbouwer Van Hool dreigt Vlaanderen 18 tot 20 miljoen euro te kosten.', '2024-04-02', 'imageEconomie3.png', 0, 'economie', ''),
-(7, 'Betaalbare psychologische zorg', 'Toegankelijke psychologische zorg: een stap naar welzijn voor iedereen.', '2024-03-09', 'imageGzondheid1.png', 0, 'gezondheid', ''),
-(8, 'De jungle van de medische platformen', 'Ontdek de weg naar jouw medische gegevens - helder, veilig en eenvoudig.', '2024-04-11', 'imageGezondheid2.png', 0, 'gezondheid', ''),
-(9, 'Duurzaamheid in jouw huis', 'Maak van je (ver)bouwproject een klimaatvriendelijke missie met deze tips voor duurzame keuzes en praktische oplossingen.', '2024-04-22', 'imageklimaat1.png', 1, 'klimaat', ''),
-(10, 'Oprichting van de Wetenschappelijke Klimaatraad', 'Het ontwerp van koninklijk besluit introduceert een onafhankelijke Wetenschappelijke Klimaatraad voor evaluatie en opvolging van het federale klimaatbeleid, met bevoegdheid tot deskundigenraadpleging en onderzoeksrichtlijnen.', '2024-04-05', 'imageKlimaat2.png', 0, 'klimaat', '');
+(1, 'Invoering elektronische procesvoering voor het Grondwettelijk Hof', 'De ministerraad keurt een ontwerp van koninklijk besluit goed voor de eerste fase van elektronische procesvoering bij het Grondwettelijk Hof, waarbij verzoekschriften en stukken elektronisch kunnen worden ingediend via een platform op de website van het Hof.', '29-03-2024', 'image1Politiek.png', 0, 'politiek', ''),
+(2, 'Verkiezingen', 'Jouw stem telt. Met die drie woorden zullen heel wat politieke partijen jou proberen te overtuigen om op hen te stemmen bij de verkiezingen van 9 juni. Eens iedereen gestemd heeft, worden de stemmen omgezet in zetels. In het federaal parlement zijn er bijvoorbeeld 150 zetels, in het Vlaams 124, in het Brussels 89.', '06-04-2024', 'image2politiek.png', 1, 'politiek', 'Verkiezingen.php'),
+(3, 'Europese verkiezingen', 'Op 9 juni 2024 worden 150 leden van de Kamer van Volksvertegenwoordigers verkozen in elf kieskringen, één per provincie en de kieskring Brussel-Hoofdstad. De 60 leden van de Senaat worden niet rechtstreeks gekozen, maar via de deelstaatparlementen. Deze verkiezingen markeren het begin van de 56ste zittingsperiode van het Federaal Parlement van België.', '06-04-2024', 'image3Politiek.png', 0, 'politiek', ''),
+(4, 'Begroting ', 'België sluit 2023 af met een begrotingstekort dat 6,5 miljard euro kleiner is dan gevreesd, vooral dankzij een sterkere economische groei, maar het tekort blijft nog steeds oplopen in vergelijking met voorgaande jaren.', '04-02-2024', 'imageEconomie1.png', 1, 'economie', 'begroting.php'),
+(5, 'De pensioenhervorming is gestemd', 'RSVZ en de Pensioendienst lanceren proactieve campagne voor pensioenbonus met slogan \'En jij, hoeveel doe jij erbij?\'', '05-04-2024', 'imageEconomie2.png', 0, 'economie', ''),
+(6, 'Faillissement Van Hool', 'Faillissement van busbouwer Van Hool dreigt Vlaanderen 18 tot 20 miljoen euro te kosten.', '02-04-2024', 'imageEconomie3.png', 0, 'economie', ''),
+(7, 'Betaalbare psychologische zorg', 'Toegankelijke psychologische zorg: een stap naar welzijn voor iedereen.', '09-03-2024', 'imageGzondheid1.png', 0, 'gezondheid', ''),
+(8, 'De jungle van de medische platformen', 'Ontdek de weg naar jouw medische gegevens - helder, veilig en eenvoudig.', '11-04-2024', 'imageGezondheid2.png', 0, 'gezondheid', ''),
+(9, 'Duurzaamheid in jouw huis', 'Maak van je (ver)bouwproject een klimaatvriendelijke missie met deze tips voor duurzame keuzes en praktische oplossingen.', '22-04-2024', 'imageklimaat1.png', 1, 'klimaat', ''),
+(10, 'Oprichting van de Wetenschappelijke Klimaatraad', 'Het ontwerp van koninklijk besluit introduceert een onafhankelijke Wetenschappelijke Klimaatraad voor evaluatie en opvolging van het federale klimaatbeleid, met bevoegdheid tot deskundigenraadpleging en onderzoeksrichtlijnen.', '05-04-2024', 'imageKlimaat2.png', 0, 'klimaat', '');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `favorieten`
+-- Table structure for table `favorieten`
 --
 
 CREATE TABLE `favorieten` (
@@ -69,7 +69,7 @@ CREATE TABLE `favorieten` (
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -82,7 +82,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Gegevens worden geëxporteerd voor tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `profiel_foto`) VALUES
@@ -90,17 +90,17 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `profie
 (2, 'Hannelore', 'Van Buynderen', 'hannelorevb@hotmail.be', '$2y$10$jONLgGT/E9eKq4LT58XP2uJlirIBu/QOzN8D.uvUDkMey5MLVxs5i', '');
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `artikel`
+-- Indexes for table `artikel`
 --
 ALTER TABLE `artikel`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `favorieten`
+-- Indexes for table `favorieten`
 --
 ALTER TABLE `favorieten`
   ADD PRIMARY KEY (`id`),
@@ -108,33 +108,33 @@ ALTER TABLE `favorieten`
   ADD KEY `artikel_id` (`artikel_id`);
 
 --
--- Indexen voor tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `artikel`
+-- AUTO_INCREMENT for table `artikel`
 --
 ALTER TABLE `artikel`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT voor een tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Beperkingen voor geëxporteerde tabellen
+-- Constraints for dumped tables
 --
 
 --
--- Beperkingen voor tabel `favorieten`
+-- Constraints for table `favorieten`
 --
 ALTER TABLE `favorieten`
   ADD CONSTRAINT `favorieten_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
