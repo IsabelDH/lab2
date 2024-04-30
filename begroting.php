b56fce376b0f29b5e83ecce2e31f1fd63c2fde81
IsabelDH <isabel.de.haes@hotmail.com>
2024-04-30 15:41:22 +0200

    begroting page klarr

--- begroting.php
+++ begroting.php
@@ -4,20 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Begroting</title>
+    <link rel="stylesheet" href="styles/begroting.css">
+    <link rel="stylesheet" href="styles/general.css">
 </head>
 <body>
-
+<?php include_once("nav.inc.php") ?>
     <h1>Begroting</h1>
 
-    <div>
-        <img src="images/begroting.jpg" alt="">
+    <div class="begroting">
+        <img src="images/begrotingpage.png" alt="">
     </div>
-
+ <div class="div">
     <div>
         <h2>Europees niveau</h2>
-        <p>De EU-begroting, ook bekend als het meerjarig financieel kader (MFK), is een langetermijnuitgavenplan voor de Europese Unie dat voor een periode van vijf tot zeven jaar wordt vastgesteld. Het MFK definieert de prioriteiten en grenzen voor de EU-uitgaven. Het grootste deel van de begroting gaat naar investeringen, variërend van plattelandsontwikkeling en milieubehoud tot de bescherming van de buitengrenzen en mensenrechten.
-            Het beheer van het EU-geld wordt gedeeld tussen nationale autoriteiten en de Europese Commissie (75%), de Europese Commissie en haar agentschappen (18%), en andere internationale organisaties, nationale agentschappen of niet-EU-landen (8%). Binnen de huidige begroting (2021-2027) wordt 90% van de middelen voor NextGenerationEU via een direct beheerde faciliteit voor herstel en veerkracht verstrekt.
-            De begroting wordt opgesteld door de Europese Commissie, de Raad en het Europees Parlement gezamenlijk. De Commissie stelt een ontwerpbegroting voor, die wordt aangepast door de Raad en het Parlement. Uitgaven van de EU worden verdeeld over zeven categorieën, met prioriteiten zoals economische, sociale en territoriale cohesie, het aanpakken van de gevolgen van internationale crises zoals de oorlog in Oekraïne, duurzaam herstel na COVID-19, digitale transitie, en veiligheid.
+        <p>De EU-begroting, ook bekend als het meerjarig financieel kader (MFK), is een langetermijnuitgavenplan voor de Europese Unie dat voor een periode van vijf tot zeven jaar wordt vastgesteld. Het MFK definieert de prioriteiten en grenzen voor de EU-uitgaven. Het grootste deel van de begroting gaat naar investeringen, variërend van plattelandsontwikkeling en milieubehoud tot de bescherming van de buitengrenzen en mensenrechten. <br> <br>
+            Het beheer van het EU-geld wordt gedeeld tussen nationale autoriteiten en de Europese Commissie (75%), de Europese Commissie en haar agentschappen (18%), en andere internationale organisaties, nationale agentschappen of niet-EU-landen (8%). Binnen de huidige begroting (2021-2027) wordt 90% van de middelen voor NextGenerationEU via een direct beheerde faciliteit voor herstel en veerkracht verstrekt. <br> <br>
+            De begroting wordt opgesteld door de Europese Commissie, de Raad en het Europees Parlement gezamenlijk. De Commissie stelt een ontwerpbegroting voor, die wordt aangepast door de Raad en het Parlement. Uitgaven van de EU worden verdeeld over zeven categorieën, met prioriteiten zoals economische, sociale en territoriale cohesie, het aanpakken van de gevolgen van internationale crises zoals de oorlog in Oekraïne, duurzaam herstel na COVID-19, digitale transitie, en veiligheid. <br> <br>
             Dit uitgavenplan stelt de EU in staat haar financieringsprogramma's enkele jaren vooruit te plannen en gerichte investeringen te doen die de Europese economie en samenleving versterken.
         </p>
     </div>
@@ -25,16 +27,16 @@
     <div>
         <h2>Federaal niveau</h2>
         <p>
-            De begroting van de overheid is eigenlijk een plan voor hoe de overheid haar geld zal uitgeven en waar dat geld vandaan zal komen. Het is een beetje zoals het budget dat je maakt om te beslissen hoe je je geld wilt uitgeven. Stel je voor dat je elke maand een bepaald bedrag aan geld verdient. Met dat geld moet je al je kosten dekken, zoals eten, huisvesting, vervoer, enzovoort. De begroting van de overheid werkt op dezelfde manier, maar op een veel grotere schaal.
-            De overheid ontvangt geld uit verschillende bronnen, zoals belastingen, leningen en soms uit investeringen. Met dat geld financiert de overheid verschillende uitgaven, zoals gezondheidszorg, onderwijs, infrastructuur, defensie en sociale programma's.
-            De begroting laat zien hoeveel geld de overheid van plan is uit te geven en aan welke dingen. Het is belangrijk dat de inkomsten van de overheid gelijk zijn aan of groter zijn dan de uitgaven, zodat er geen tekort ontstaat. Als er wel een tekort is, moet de overheid misschien geld lenen, wat later terugbetaald moet worden met rente.
-            Het doel van een begroting is om ervoor te zorgen dat de overheid haar geld op een verantwoorde en effectieve manier gebruikt om de behoeften van de samenleving te vervullen en de economie te stimuleren.
-            België sloot 2023 af met een tekort van 26,7 miljard euro, wat 6,5 miljard minder is dan verwacht. Dit kwam vooral door een sterkere economische groei. Het federale tekort was 3,3% van het bbp, terwijl de deelstaten en gemeenten een tekort van 1,3% hadden. Ondanks een bovengemiddelde groei steeg het begrotingstekort in vergelijking met 2022.
+            De begroting van de overheid is eigenlijk een plan voor hoe de overheid haar geld zal uitgeven en waar dat geld vandaan zal komen. Het is een beetje zoals het budget dat je maakt om te beslissen hoe je je geld wilt uitgeven. Stel je voor dat je elke maand een bepaald bedrag aan geld verdient. Met dat geld moet je al je kosten dekken, zoals eten, huisvesting, vervoer, enzovoort. De begroting van de overheid werkt op dezelfde manier, maar op een veel grotere schaal.<br> <br>
+            De overheid ontvangt geld uit verschillende bronnen, zoals belastingen, leningen en soms uit investeringen. Met dat geld financiert de overheid verschillende uitgaven, zoals gezondheidszorg, onderwijs, infrastructuur, defensie en sociale programma's. <br> <br>
+            De begroting laat zien hoeveel geld de overheid van plan is uit te geven en aan welke dingen. Het is belangrijk dat de inkomsten van de overheid gelijk zijn aan of groter zijn dan de uitgaven, zodat er geen tekort ontstaat. Als er wel een tekort is, moet de overheid misschien geld lenen, wat later terugbetaald moet worden met rente. <br> <br>
+            Het doel van een begroting is om ervoor te zorgen dat de overheid haar geld op een verantwoorde en effectieve manier gebruikt om de behoeften van de samenleving te vervullen en de economie te stimuleren.<br> <br>
+            België sloot 2023 af met een tekort van 26,7 miljard euro, wat 6,5 miljard minder is dan verwacht. Dit kwam vooral door een sterkere economische groei. Het federale tekort was 3,3% van het bbp, terwijl de deelstaten en gemeenten een tekort van 1,3% hadden. Ondanks een bovengemiddelde groei steeg het begrotingstekort in vergelijking met 2022. <br> <br>
 
             In België vallen veel belastingen onder federale bevoegdheid. Enkele belangrijke federale belastingen zijn:
-            Personenbelasting: Dit is een belasting die wordt geheven op het inkomen van natuurlijke personen, zoals salarissen, winsten uit zelfstandige activiteiten, huurinkomsten, enzovoort.
+            Personenbelasting: Dit is een belasting die wordt geheven op het inkomen van natuurlijke personen, zoals salarissen, winsten uit zelfstandige activiteiten, huurinkomsten, enzovoort. 
             Vennootschapsbelasting: Dit is een belasting op de winst van vennootschappen en rechtspersonen die actief zijn in België.
-            Belasting over de toegevoegde waarde (btw): Dit is een algemene verbruiksbelasting die wordt toegepast op de verkoop van goederen en diensten. Het tarief kan variëren afhankelijk van het soort goederen of diensten.
+            Belasting over de toegevoegde waarde (btw): Dit is een algemene verbruiksbelasting die wordt toegepast op de verkoop van goederen en diensten. Het tarief kan variëren afhankelijk van het soort goederen of diensten.<br> <br>
             De Federale Overheidsdienst Financiën is verantwoordelijk voor het innen en beheren van deze federale belastingen. Meer informatie over federale belastingen en hun specifieke tarieven is beschikbaar op de website van <a href="https://www.belgium.be/en">Belgium.be</a>.
         </p>
     </div>
@@ -46,26 +48,35 @@
 
     <div>
         <h2>Video</h2>
-        <img src="images/video.png" alt="">
+        <img class="video" src="images/video.png" alt="">
     </div>
 
-    <div>
+    <div class="totstand">
         <h2>Wil je weten hoe het tot stand komt?</h2>
         <a href="https://www.vlaanderen.be/hoe-komt-de-vlaamse-begroting-tot-stand">Klik dan hier!</a>
-        <img src="images/begrotingrekenen.png" alt="">
+        <div>
+            <img src="images/begrotingrekenen.png" alt="">
+        </div>
+        
     </div>
 
     <div>
         <h2>Hier zijn nog enkele links, die u misschien interessant vindt!</h2>
-        <div>
-            <img src="" alt="">
-            <a href="https://www.vlaanderen.be/financien-en-begroting/begroting/begrotingsakkoorden">Begrotingsakkoorden</a>
-        </div>
-        <div>
-            <img src="" alt="">
-            <a href="https://www.vlaanderen.be/financien-en-begroting/begroting/begrotingsopmaak-en-aanpassing">Begrotingsopmaak en -aanpassing</a>
+        <div class="flex">
+            <div class="item">
+                <img class="imgbegroting" src="images/begrotingsakkoorden.png" alt="">
+                <a href="https://www.vlaanderen.be/financien-en-begroting/begroting/begrotingsakkoorden">Begrotingsakkoorden</a>
+            </div>
+            <div class="item">
+                <img class="imgbegroting" src="images/begrotingsopmaak.png" alt="">
+                <a href="https://www.vlaanderen.be/financien-en-begroting/begroting/begrotingsopmaak-en-aanpassing">Begrotingsopmaak en -aanpassing</a>
+            </div>
         </div>
     </div>
-    
+    </div> 
+    <?php include_once("footer.inc.php") ?>
+
+    <script src="js/chatbot.js"></script>
+    <script src="js/zoekbalk.js"></script>
 </body>
 </html>
